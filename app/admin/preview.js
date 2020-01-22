47d0cf8fd72a91a38de7d697281d7afb03f0e824
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-01-22 18:45:12 +0000

    Add date filter to preview template

--- app/admin/preview.js
+++ app/admin/preview.js
@@ -1,10 +1,13 @@
 /* globals utils, nunjucks, React, CMS */
 
 const {
+  date,
   markdown
 } = utils
 
 const env = nunjucks.configure()
+env.addFilter('markdown', markdown)
+env.addFilter('date', date)
 
 const Preview = ({ entry, path, context }) => {
   const data = context(entry.get('data').toJS())
@@ -19,10 +22,12 @@ const Preview = ({ entry, path, context }) => {
 const Post = ({ entry }) => React.createElement(Preview, {
   entry: entry,
   path: 'app/_layouts/admin-preview.njk',
-  context: ({ title, description, date, body }) => ({
+  context: ({ title, description, tags, date, modified, body }) => ({
     title,
     description,
+    tags,
     date,
+    modified,
     content: markdown(body || '')
   })
 })
