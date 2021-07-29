ffb2cdb09d2d5ec0e512a1e09478d74403f2803b
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-07-29 14:18:35 +0100

    Save images alongside post to enable relative image paths

--- .eleventy.js
+++ .eleventy.js
@@ -35,10 +35,8 @@ module.exports = function (eleventyConfig) {
 
   // Passthrough
   eleventyConfig.addPassthroughCopy('./app/documents')
-  eleventyConfig.addPassthroughCopy('./app/images')
-  eleventyConfig.addPassthroughCopy({
-    'node_modules/govuk-frontend/govuk/assets': 'assets'
-  })
+  eleventyConfig.addPassthroughCopy({'./app/images': '.'})
+  eleventyConfig.addPassthroughCopy({'node_modules/govuk-frontend/govuk/assets': 'assets'})
 
   // Enable data deep merge
   eleventyConfig.setDataDeepMerge(true)
