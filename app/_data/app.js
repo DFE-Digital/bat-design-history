0cc52a450b3fe3eb631c014454f86797981d380b
Simon Whatley <simonwhatley@gmail.com>
2021-11-05 09:57:14 +0000

    Add description as a variable

--- app/_data/app.js
+++ app/_data/app.js
@@ -1,5 +1,6 @@
 module.exports = {
   productName: 'Becoming a teacher design history',
+  description: 'A history of the designs for the Find, Apply, Publish, Manage, Register and Support services',
   sections: {
     title: 'Services'
   }
