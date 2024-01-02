4fe9d06487c96c9f03a1c0bf70ef1028d7e0a2bd
Simon Whatley <simonwhatley@gmail.com>
2024-01-02 14:11:26 +0000

    Update initAll in application.js

--- app/_javascripts/application.js
+++ app/_javascripts/application.js
@@ -1,3 +1,5 @@
+import { initAll } from '../../node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js'
+
 import Search from '../_components/site-search/_site-search.js'
 
 const $searchContainer = document.querySelector('[data-module="app-search"]')
@@ -5,5 +7,5 @@ new Search($searchContainer).init()
 
 window.onload = function () {
   document.documentElement.classList.remove('no-js')
-  window.GOVUKFrontend.initAll()
+  initAll()
 }
